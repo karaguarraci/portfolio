9c97bcae642e25bdcaaf33979422e56eb4882b83
Kara Guarraci <karaguarraci@gmail.com>
2023-05-30 13:47:29 +0100

    contact added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,46 @@
 const Contact = () => {
-  return <p>you have made it to the contact page</p>;
+  return (
+    <div className="contact">
+      <h2>Contact</h2>
+      <div className="contact-buttons">
+        <a
+          href="mailto:karaguarraci@gmail.com"
+          className="contact-button email-button"
+        >
+          <img
+            src="https://img.icons8.com/?size=512&id=12580&format=png"
+            alt="Email Icon"
+            className="contact-icon"
+          />
+        </a>
+
+        <a
+          href="https://www.linkedin.com/in/kara-guarraci/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="contact-button"
+        >
+          <img
+            src="https://img.icons8.com/?size=512&id=447&format=png"
+            alt="LinkedIn Icon"
+            className="contact-icon"
+          />
+        </a>
+        <a
+          href="https://github.com/karaguarraci"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="contact-button"
+        >
+          <img
+            src="https://img.icons8.com/?size=512&id=12598&format=png"
+            alt="GitHub Icon"
+            className="contact-icon"
+          />
+        </a>
+      </div>
+    </div>
+  );
 };
+
 export default Contact;
